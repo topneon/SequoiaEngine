121c86e8fa0efd7bccb976ee9ed93b6f7f573913
topneon <kirilys75@gmail.com>
2023-02-14 21:15:59 +0100

    bug fix

--- crypter/crypter.cpp
+++ crypter/crypter.cpp
@@ -247,14 +247,14 @@ int main(int argc, char* argv[]) {
 #ifdef USE_VECTORS
 			// Get the rest of division by 32 and add that to a total file size for SIMD operations
 			// uint8_t leftover = sz % 32;
-			uint8_t leftover = sz - (sz >> 5);
+			uint8_t leftover = sz - ((sz >> 5) << 5);
 			// 
 			const auto totale = sz + 32 - leftover;
 			// Create string for input
 			string str(totale, 32 - leftover);
 #else
 			// Same as for SIMD
-			uint8_t leftover = sz - (sz >> 3);
+			uint8_t leftover = sz - ((sz >> 3) << 3);
 			const auto totale = sz + 8 - leftover;
 			string str(totale, 8 - leftover);
 #endif // USE_VECTORS
